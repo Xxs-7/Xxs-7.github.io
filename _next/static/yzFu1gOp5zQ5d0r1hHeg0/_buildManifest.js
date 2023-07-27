d1440f06bb1775575d3866b9a5dc099ecd6de7d0
Xxs-7 <852953252@qq.com>
2023-07-27 19:54:09 +0000

    Deploying to gh-pages from @ Xxs-7/Xxs-7.github.io@10632f80a1c3fc553a782755a7eb17bff6ef0422 🚀

---
+++ _next/static/yzFu1gOp5zQ5d0r1hHeg0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-72203bf2b4ec446d.js"],"/_error":["static/chunks/pages/_error-9aab326ddb65e35d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
