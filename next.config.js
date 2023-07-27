ec05793a3b28c3a6a11707a00f3d20aba3e9bce9
xuxunsheng <xunsheng.xu@everxyz.com>
2023-07-28 05:05:34 +0800

    test

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = "";
 let basePath = "";
 
-if (isGithubActions) {
-  // 去掉 `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+// if (isGithubActions) {
+//   // 去掉 `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
+//   assetPrefix = `/${repo}/`;
+//   basePath = `/${repo}`;
+// }
 
 const nextConfig = {
   output: "export",
