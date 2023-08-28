13cf72d73ca6730300db2c49bd12f2e222ac2314
xuxunsheng <xunsheng.xu@everxyz.com>
2023-08-28 20:02:05 +0800

    feature: font optimize

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,3 @@
-import Layout from "@/components/Layout";
-
 export default function NotFound() {
   return (
     <p className="mdx-heading text-3xl font-extrabold my-6 ">Page not found</p>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,16 @@
 import Layout from "@/components/Layout";
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import localFont from "next/font/local";
+import "@/styles/globals.css";
 import React from "react";
 
-// Font files can be colocated inside of `app`
-const CustomeFont = localFont({
-  src: [
-    {
-      path: "./../styles/font/LXGWWenKai-Light.ttf",
-      style: "normal",
-      weight: "400",
-    },
-    {
-      path: "./../styles/font/LXGWWenKai-Bold.ttf",
-      style: "normal",
-      weight: "700",
-    },
-  ],
-});
-
-// If loading a variable font, you don't need to specify the font weight
+import "@chinese-fonts/lxgwwenkai/dist/LXGWWenKai-Regular/result.css";
+import "@chinese-fonts/lxgwwenkai/dist/LXGWWenKai-Bold/result.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={CustomeFont.className}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </div>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
