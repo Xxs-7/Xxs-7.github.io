684a571f9718fd93ebb1525a0c404f76b21158ee
xuxunsheng <xunsheng.xu@everxyz.com>
2024-03-29 17:13:18 +0800

    add: stacking-context

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -54,21 +54,6 @@ const MyDocument = () => {
               `,
           }}
         />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-5PX24PZR11"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-5PX24PZR11');
-            `,
-          }}
-        />
         <Main />
         <NextScript />
       </body>
